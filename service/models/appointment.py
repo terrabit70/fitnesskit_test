5acf869f4fa9c10c0c3507dd6b27c931513c7184
terrabit70 <ivgen_hunt@mail.ru>
2020-05-12 17:24:05 +0700

    WIP

--- service/models/appointment.py
+++ service/models/appointment.py
@@ -27,4 +27,4 @@ class Appointment(models.Model):
     teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL,  blank=False, null=True)
 
     def __str__(self):
-        return f'{self.service} : {self.weekday}'
+        return f'{self.service} : weekday {self.weekday}'
