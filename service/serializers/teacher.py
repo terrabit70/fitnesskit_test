9037f1da5d6d92edb47e59d9580aa171adf81b2f
terrabit70 <ivgen_hunt@mail.ru>
2020-05-12 16:55:10 +0700

    WIP

--- service/serializers/teacher.py
+++ service/serializers/teacher.py
@@ -8,7 +8,10 @@ class TeacherSerializer(serializers.ModelSerializer):
     imageUrl = serializers.SerializerMethodField()
 
     def get_imageUrl(self, instance):
-        return f'http://{socket.gethostbyname(socket.gethostname())}{settings.MEDIA_URL}{instance.image}'
+        if not f'{instance.image}':
+            return 'qweqweqwe'
+        else:
+            return f'http://{socket.gethostbyname(socket.gethostname())}{settings.MEDIA_URL}{instance.image}'
 
     class Meta:
         model = Teacher
