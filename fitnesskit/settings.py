56010259ecd69ece85f399e0c4b1f21e25d7f5f2
terrabit70 <ivgen_hunt@mail.ru>
2020-05-12 14:49:38 +0700

    WIP

--- fitnesskit/settings.py
+++ fitnesskit/settings.py
@@ -17,23 +17,18 @@ import yaml
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(os.path.join(BASE_DIR, 'config.yaml')) as f:
-    config = yaml.safe_load(f)
-TUNNEL_HOST = config.get('tunnel_host')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('secret_key')
+SECRET_KEY = 'o(%1+!@94w6n)qa^qzi82kc6bobhump92@95j)z53wgvrn)3)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['0.0.0.0']
-if TUNNEL_HOST:
-    ALLOWED_HOSTS.append(TUNNEL_HOST)
+ALLOWED_HOSTS = []
 
 
 # Application definition
