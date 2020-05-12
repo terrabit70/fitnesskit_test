3162f50f9d3b87608316ea8729844106190a67de
terrabit70 <ivgen_hunt@mail.ru>
2020-05-12 15:07:12 +0700

    WIP

--- fitnesskit/settings.py
+++ fitnesskit/settings.py
@@ -149,8 +149,8 @@ if os.getcwd() == '/app':
 
     # Static resources configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
-    )
+#    STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, '/static/'),
+#    )
